e64b34c8850869a797cadf3e3569ff36d3fc7d4d
l00tus <marioalexandruu04@gmail.com>
2024-02-24 18:17:42 +0200

    Handle valid letters and backspaces

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// tba
+const letters = document.querySelectorAll(".letter");
+let currentLetter = 0;
+
+document.addEventListener("keydown", function(event) {
+    console.log(event.key);
+    if (isLetter(event.key)) {
+        letters[currentLetter++].textContent = event.key.toUpperCase();
+    } else if(event.key === "Backspace") {
+        if(currentLetter !== 0) {
+            letters[--currentLetter].textContent = "";
+        }
+    }
+
+});
+
+function isLetter(letter) {
+    return /^[a-zA-Z]$/.test(letter);
+}
